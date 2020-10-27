e13d8eecf8ce4c077622b99d7580e393c71a985d
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-10-27 18:03:05 -0400

    brilliant

--- src/Components/Quesion.js
+++ src/Components/Quesion.js
@@ -1,34 +1,42 @@
 import React from 'react'
 
-const Question = props => {
+class Question extends React.Component {
 
-    const correctAnswer = e => {
+    state = {
+        answered: false 
+    }
+
+     correctAnswer = e => {
         e.preventDefault()
-        if (e.target.name == props.correct){
+        if (e.target.name == this.props.correct){
             e.target.style.backgroundColor = 'Green'
-            props.addToScore(e)
+            this.props.addToScore(e)
         } else {
             e.target.style.backgroundColor = 'Red'
         }
+        this.setState({ answered: true })
     }
 
-    const shuffleArray = array => {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
+    //  shuffleArray = a => {
+    //     for (let i = a.length - 1; i > 0; i--) {
+    //         let j = Math.floor(Math.random() * (i + 1));
+    //         let x = a[i];
+    //         a[i] = a[j];
+    //         a[j] = x;
+    //     }
+    //     return a;
+    // }
     
-    const possibleAnswers = [...props.incorrect, props.correct]
-    shuffleArray(possibleAnswers)
+    render(){
+       const possibleAnswers = [...this.props.incorrect, this.props.correct]
+       possibleAnswers.sort()
     return (
         <div>
-            <h3>{props.question}</h3>
-            {possibleAnswers.map((answer, index) => <div><button onClick={correctAnswer} name={answer} key={index}>{answer}</button><br></br></div> )}
+            <h3>{this.props.question}</h3>
+            {possibleAnswers.map((answer, index) => <div><button onClick={this.correctAnswer} name={answer} key={index}>{answer}</button><br></br></div> )}
         </div>
     )
+  }
 }
 
 export default Question
