8361adc811714f3d897a197e9ef170a24d0c39cd
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-10-28 18:10:09 -0400

    gettingSomeWhere

--- src/Components/Quesion.js
+++ src/Components/Quesion.js
@@ -13,12 +13,23 @@ const Question = props => {
     }
 
     const alreadyAnswered = () => alert('QUESTION ALREADY ANSWERED')
+
+    //{answerGiven === answer ? style.backgroundColor = 'Lime' : style.backgroundColor = 'Red'}
     
     if (!props.incorrect) return <div>Loading...</div>
        const possibleAnswers = [...props.incorrect, props.correct]
+       const answerGiven = props.answerGiven
+       console.log(answerGiven)
        possibleAnswers.sort()
-    if (props.answered) return (
-        <div>
+    if (props.answered && props.answerGiven === props.correct) return (
+        <div id='green'>
+            <p id='back' onClick={()=>props.removeQuestion(props.index)}>X</p>
+            <p id='question'>{props.question}</p>
+            {possibleAnswers.map((answer, index) => <div><button onClick={alreadyAnswered} name={answer} key={index}>{answer}</button><br></br></div> )}
+        </div>
+    )
+    if (props.answered && props.answerGiven != props.correct) return (
+        <div id='red'>
             <p id='back' onClick={()=>props.removeQuestion(props.index)}>X</p>
             <p id='question'>{props.question}</p>
             {possibleAnswers.map((answer, index) => <div><button onClick={alreadyAnswered} name={answer} key={index}>{answer}</button><br></br></div> )}
