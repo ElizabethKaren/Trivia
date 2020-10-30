680b680349f6dac13888c89d8e2f460cc8e8f90d
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-10-30 10:17:14 -0400

    beginChangedToStart

--- src/Components/UserName.js
+++ src/Components/UserName.js
@@ -7,7 +7,7 @@ const UserName = props => {
             <h3>Enter Username</h3>
             <input onChange={props.handleOnChange} placeholder='Player...'></input>
             <br></br><br></br>
-            <button onClick={props.updateUser}>Begin</button>
+            <button onClick={props.updateUser}>Start</button>
             <h3>Ten Quesitons A Game! Win up to 50 points!</h3>
         </div>
     )
